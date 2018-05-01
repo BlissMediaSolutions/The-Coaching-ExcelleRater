3f47da409eea82ae3ce46e792d84f0ec5b74781d
Luke Badawy <luke.badawy@redhotminute.com>
2018-05-01 13:53:58 +1000

    attempeted to resolve URL issue on server for real....

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename="/coaching">
     <App />
   </BrowserRouter>
   , document.getElementById('root'));
