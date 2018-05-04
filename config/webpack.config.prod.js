68a21f47ab82817bab9fe736501536356b5139aa
Luke Badawy <luke.badawy@redhotminute.com>
2018-05-04 15:27:34 +1000

    added changed to webpack prod file to attempt to get styles to load on prod enviroment

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -153,6 +153,13 @@ module.exports = {
               compact: true,
             },
           },
+          //Adds Support for SCSS and Hot reloading for it on save
+          // not native to create react app
+          {
+            test: /\.scss$/,
+            include: paths.appSrc,
+            use: ['style-loader', 'css-loader?sourceMap', 'sass-loader?sourceMap=true']
+          },
           // The notation here is somewhat confusing.
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
@@ -222,7 +229,7 @@ module.exports = {
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.sass$/, /\.scss$/],
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
             },
