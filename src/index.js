e31a7a9721341eca90c9ee01cf3615634d055e04
Luke Badawy <luke.badawy@redhotminute.com>
2018-05-09 14:24:14 +1000

    added more redux thing, added commend out ajax call and added code for toolbox-menu with animation

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename="/coaching">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>
--- src/react/pages/login.js
+++ src/react/pages/login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Banner from '../components/banner';
+import { NavLink } from 'react-router-dom';
 
 class Login extends Component {
   constructor(props) {
@@ -45,6 +46,41 @@ class Login extends Component {
         <Banner title="Login" bgImage="https://i.pinimg.com/originals/44/7e/2e/447e2e8f27045f2ec24eb0d7d4e2e1ea.png"></Banner>
           <div className="container">
             <div className="u-component">
+              <h2>Test for toolbox animation</h2>
+              <div>
+                <nav className="menu">
+                  <input type="checkbox" onClick={this.clickHandler} href="#" className="menu-open" name="menu-open" id="menu-open"/>
+                  <label className="menu-open-button" htmlFor="menu-open">
+                    <span className="hamburger hamburger-1"></span>
+                    <span className="hamburger hamburger-2"></span>
+                    <span className="hamburger hamburger-3"></span>
+                  </label>
+                  
+                  <NavLink exact activeClassName="active" to="/" className="menu-item"> <i className="fa fa-home"></i> </NavLink>
+                  <NavLink activeClassName="active" to="/articles" className="menu-item"> <i className="fa fa-newspaper-o"></i> </NavLink>
+                  <NavLink activeClassName="active" to="/home" className="menu-item"> <i className="fa fa-heart"></i> </NavLink>
+                  <NavLink activeClassName="active" to="/link3" className="menu-item"> <i className="fa fa-envelope"></i> </NavLink>    
+                </nav>
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                  <defs>
+                    <filter id="shadowed-goo">
+                        
+                        <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
+                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+                        <feGaussianBlur in="goo" stdDeviation="3" result="shadow" />
+                        <feColorMatrix in="shadow" mode="matrix" values="0 0 0 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 1 -0.2" result="shadow" />
+                        <feOffset in="shadow" dx="1" dy="1" result="shadow" />
+                        <feComposite in2="shadow" in="goo" result="goo" />
+                        <feComposite in2="goo" in="SourceGraphic" result="mix" />
+                    </filter>
+                    <filter id="goo">
+                        <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
+                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+                        <feComposite in2="goo" in="SourceGraphic" result="mix" />
+                    </filter>
+                  </defs>
+                </svg>
+              </div>
               <h2>Log into an existing account</h2>
               <div className="u-form-container">
                 <div className="row mb-3">
