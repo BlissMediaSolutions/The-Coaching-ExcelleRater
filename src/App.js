15f073727b4de218978a9de6c42b0129e1f5ec13
jacksonz123 <37988182+jacksonz123@users.noreply.github.com>
2018-08-04 10:10:42 +1000

    Added Graphcool with Apollo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
+// React Apollo Set up for communicating to the backend
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import gql from "graphql-tag";
+
 // Styles
 // You can import styles into JS the way  have set this up because webpack
 // will see this import and know what to do. they will get rendered out to
@@ -24,23 +29,41 @@ import CreateWorkflow from './react/pages/createWorkflow';
 import Nav from './react/components/nav';
 import Footer from './react/components/footer';
 
+const client = new ApolloClient({
+  uri: "https://api.graph.cool/simple/v1/cjken9aza3m0w01431phhi3v0"
+});
+
 class App extends Component {
   render() {
+
+    client
+      .query({
+        query: gql`
+          {
+            hello {
+              message
+            }
+          }
+        `
+      })
+      .then(result => console.log(result));
+
     console.log(process.env.PUBLIC_URL);
     return (
-      <div className="App">
-        <Nav></Nav>
-        <div>
-          <Route path={`${process.env.PUBLIC_URL}/`} exact component={Home}/>
-          <Route path={`${process.env.PUBLIC_URL}/about`} exact component={About}/>
-          <Route path={`${process.env.PUBLIC_URL}/login`} exact component={Login}/>
-          <Route path={`${process.env.PUBLIC_URL}/signup`} exact component={Signup}/>
-          <Route path={`${process.env.PUBLIC_URL}/videos`} exact component={Video}/>
-          <Route path={`${process.env.PUBLIC_URL}/createWorkflow`} exact component={CreateWorkflow}/>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <Nav></Nav>
+          <div>
+            <Route path={`${process.env.PUBLIC_URL}/`} exact component={Home}/>
+            <Route path={`${process.env.PUBLIC_URL}/about`} exact component={About}/>
+            <Route path={`${process.env.PUBLIC_URL}/login`} exact component={Login}/>
+            <Route path={`${process.env.PUBLIC_URL}/signup`} exact component={Signup}/>
+            <Route path={`${process.env.PUBLIC_URL}/videos`} exact component={Video}/>
+            <Route path={`${process.env.PUBLIC_URL}/createWorkflow`} exact component={CreateWorkflow}/>
+          </div>
+          <Footer></Footer> 
         </div>
-        <Footer></Footer> 
-      </div>
-      
+      </ApolloProvider>
     );
   }
 }
