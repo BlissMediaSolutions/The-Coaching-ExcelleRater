0c605a627e7008c496dcb56a74872ac38b620ef2
Jackson Zaloumis <>
2018-08-29 10:54:19 +1000

    Preloader Fixes

--- src/react/components/common/preloader.js
+++ src/react/components/common/preloader.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-class Preloader extends Component {
-render() {
+class Preloader extends React.Component {
+  render() {
     return (
-        <div id="loader-wrapper">
-        <div id="loader"></div>
-        
-        <div class="loader-section section-left"></div>
-        <div class="loader-section section-right"></div>
-        </div>       
-    )
-}
+      <div id="loader-wrapper">
+        <div id="loader" />
+
+        <div class="loader-section section-left" />
+        <div class="loader-section section-right" />
+      </div>
+    );
+  }
 }
 export default Preloader;
