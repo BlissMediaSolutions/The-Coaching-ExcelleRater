cb56cf3f65982cd306f9d3ba6078fa63104e52d0
Danielle Walker <danielle@bliss.net.au>
2018-09-02 16:05:45 +1000

    Adding dummy data to SQL for Workflow

--- sql/new-coaching.sql
+++ sql/new-coaching.sql
@@ -144,6 +144,14 @@ INSERT INTO teamlist(username, password, teamid, personid, roleid) VALUES ('grid
 INSERT INTO teamlist(username, password, teamid, personid, roleid) VALUES ('lgaravito', '5f4dcc3b5aa765d61d8327deb882cf99', 1, 6, 2);
 INSERT INTO teamlist(username, password, teamid, personid, roleid) VALUES ('plopez', '5f4dcc3b5aa765d61d8327deb882cf99', 1, 7, 2);
 
-INSERT INTO video(teamid, description, private, filename, category1, category2, category3, title) VALUES (1, 'Where to Kick after Point Scored?', 1, 'Clip01N.mp4', 'W Bulldogs', '', '', 'WB Kickout');
-INSERT INTO video(teamid, description, private, filename, category1, category2, category3, title) VALUES (1, 'Where to Kick after Point Scored?', 1, 'Clip02N.mp4', 'W Bulldogs', '', '', 'WB Kickout');
-INSERT INTO video(teamid, description, private, filename, category1, category2, category3, title) VALUES (2, 'Where to Kick after Point Scored?', 1, 'Clip03N.mp4', 'W Bulldogs', '', '', 'WB Kickout');
+INSERT INTO video(teamid, description, private, filename, category1, category2, category3, title) VALUES (1, 'Where to Kick after Point Scored?', 1, 'Clip01N.mp4', 'W Bulldogs', '', '', 'WB Kickout1');
+INSERT INTO video(teamid, description, private, filename, category1, category2, category3, title) VALUES (1, 'Where to Kick after Point Scored?', 1, 'Clip02N.mp4', 'W Bulldogs', '', '', 'WB Kickout2');
+INSERT INTO video(teamid, description, private, filename, category1, category2, category3, title) VALUES (2, 'Where to Kick after Point Scored?', 1, 'Clip03N.mp4', 'W Bulldogs', '', '', 'WB Kickout3');
+
+INSERT INTO workflow (wfdate, wfname, teamid, coachid, resolution) VALUES (2018-08-29, 'Workflow 1', '1', '1', '800x600');
+INSERT INTO workflow (wfdate, wfname, teamid, coachid, resolution) VALUES (2018-08-30, 'Workflow 2', '1', '1', '800x600');
+INSERT INTO workflow (wfdate, wfname, teamid, coachid, resolution) VALUES (2018-08-31, 'Workflow 3', '1', '1', '800x600');
+
+INSERT INTO wfteamlist (workflowid, personid, totalscore) VALUES ('1', '2', '');
+INSERT INTO wfteamlist (workflowid, personid, totalscore) VALUES ('2', '2', '');
+INSERT INTO wfteamlist (workflowid, personid, totalscore) VALUES ('3', '2', '');
