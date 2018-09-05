afa751da2dc9299ac8983f1c260fb67f286281ea
Jackson Zaloumis <>
2018-09-05 17:23:19 +1000

    Fix Flow to have no nav

--- src/react/pages/videoFlow.js
+++ src/react/pages/videoFlow.js
@@ -3,15 +3,12 @@ import axios from "axios";
 import { compose, graphql } from "react-apollo";
 import { videoFlowQuery, updateVideoFlow } from "../../graphql/videoFlow";
 
-import { Button } from "reactstrap";
 import Banner from "../components/common/banner";
 import SuccessModal from "../components/common/successModal";
 import Preloader from "../components/common/preloader";
 import { SelectWorkflow, Video } from "../components/videoFlow";
 
 import * as types from "../../graphql/types";
-
-const maxIndex = 1;
 
 class VideoFlow extends Component {
   constructor(props) {
@@ -41,38 +38,10 @@ class VideoFlow extends Component {
       });
   }
 
-  /* Generic Functions */
-  onNextClick = () => {
-    this.setState({
-      index: this.state.index + 1
-    });
-  };
-
-  onPrevClick = () => {
-    this.setState({
-      index: this.state.index - 1
-    });
-  };
-
   onChange = e => {
     this.setState({
       [e.target.name]: e.target.value
     });
-  };
-
-  isComplete(index) {
-    switch (index) {
-      case 0:
-        return this.isSelectWorkflowComplete();
-      default:
-        return false;
-    }
-  }
-
-  /* Workflow Related Functions */
-  isSelectWorkflowComplete = () => {
-    // Force user to select video
-    return true; // TO DO: Fix this
   };
 
   // Stops the player at the required Time Stamps
@@ -202,8 +171,7 @@ class VideoFlow extends Component {
 
   render() {
     const { loading, index } = this.state;
-    const canNext = this.isComplete(index) && index < maxIndex;
-    const canPrev = index !== 0;
+    const showBanner = index === 0;
 
     if (loading) {
       return <Preloader />;
@@ -211,7 +179,7 @@ class VideoFlow extends Component {
 
     return (
       <div>
-        {!canPrev && (
+        {showBanner && (
           <Banner
             title="Create Workflow"
             bgImage="https://i.pinimg.com/originals/44/7e/2e/447e2e8f27045f2ec24eb0d7d4e2e1ea.png"
@@ -221,24 +189,6 @@ class VideoFlow extends Component {
           <div className="row justify-content-center">
             {this.componentToRender(index)}
           </div>
-          {index !== 4 && (
-            <div className="row justify-content-between mb-4">
-              <Button
-                color="secondary"
-                onClick={this.onPrevClick}
-                disabled={!canPrev}
-              >
-                Previous
-              </Button>
-              <Button
-                color="primary"
-                onClick={this.onNextClick}
-                disabled={!canNext}
-              >
-                Next
-              </Button>
-            </div>
-          )}
         </div>
       </div>
     );
