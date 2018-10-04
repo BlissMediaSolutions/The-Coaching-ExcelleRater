19969a3e0e22e7029cbc15be7bb02b5ad03fae31
Deven_05 <deven.naidoo@hotmail.com>
2018-10-04 13:42:09 +1000

    Added text to about page

--- src/react/pages/about.js
+++ src/react/pages/about.js
@@ -12,20 +12,29 @@ class About extends Component {
         <div className="container">
           <div className="u-component">
             <h2>How to contact us?</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              facere animi quod excepturi deserunt ratione eos ipsa? Laudantium
-              nobis eligendi sit libero possimus necessitatibus nulla, quam
-              tenetur aliquid incidunt obcaecati voluptates ipsa, magni,
-              praesentium
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              facere animi quod excepturi deserunt ratione eos
-            </p>
-          </div>
-        </div>
+<p>
+  The Coaching ExcelleRater is a video-based decision-making platform for performance analysis and coaching.
+</p>
+
+<p> 
+  Identifying, classifying and rating video clips has become a crucial component of many professional sports coaching programs.
+</p>
+
+<p> 
+  The Coaching ExcelleRater allows coaching staff to select relevant video clips and customise decision-making scenarios for their athletes.
+</p>
+
+<p>
+        Features include:
+    <ul> 
+<li> A simple 3-2-1 scoring system, with user-defined regions indicating a set of good decisions. </li>
+<li> Real-time and above real-time video playback, to better simulate in-game cognitive demands.</li>
+<li> Separate athlete and coach roles, providing access to the decision-making scenarios and summary data. </li>
+    </ul>
+</p>
       </div>
+    </div>
+</div>
     );
   }
 }
