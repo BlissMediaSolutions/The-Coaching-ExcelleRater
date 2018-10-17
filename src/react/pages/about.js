0df46ef063ac65e009cf927ef130f0f94c0f0ccd
Jackson Zaloumis <>
2018-10-17 17:22:17 +1100

    Home Page Text Fix

--- src/react/pages/about.js
+++ src/react/pages/about.js
@@ -12,29 +12,42 @@ class About extends Component {
         <div className="container">
           <div className="u-component">
             <h2>What we do</h2>
-<p>
-  The Coaching ExcelleRater is a video-based decision-making platform for performance analysis and coaching.
-</p>
+            <p>
+              The Coaching ExcelleRater is a video-based decision-making
+              platform for performance analysis and coaching.
+            </p>
 
-<p> 
-  Identifying, classifying and rating video clips has become a crucial component of many professional sports coaching programs.
-</p>
+            <p>
+              Identifying, classifying and rating video clips has become a
+              crucial component of many professional sports coaching programs.
+            </p>
 
-<p> 
-  The Coaching ExcelleRater allows coaching staff to select relevant video clips and customise decision-making scenarios for their athletes.
-</p>
+            <p>
+              The Coaching ExcelleRater allows coaching staff to select relevant
+              video clips and customise decision-making scenarios for their
+              athletes.
+            </p>
 
-<p>
-        Features include:
-    <ul> 
-<li> A simple 3-2-1 scoring system, with user-defined regions indicating a set of good decisions. </li>
-<li> Real-time and above real-time video playback, to better simulate in-game cognitive demands.</li>
-<li> Separate athlete and coach roles, providing access to the decision-making scenarios and summary data. </li>
-    </ul>
-</p>
+            <p>
+              Features include:
+              <ul>
+                <li>
+                  A simple 3-2-1 scoring system, with user-defined regions
+                  indicating a set of good decisions.
+                </li>
+                <li>
+                  Real-time and above real-time video playback, to better
+                  simulate in-game cognitive demands.
+                </li>
+                <li>
+                  Separate athlete and coach roles, providing access to the
+                  decision-making scenarios and summary data.
+                </li>
+              </ul>
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
-</div>
     );
   }
 }
--- src/react/pages/home.js
+++ src/react/pages/home.js
@@ -13,44 +13,37 @@ class Home extends Component {
           <div className="u-component">
             <h2>What is coaching excellerator?</h2>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              facere animi quod excepturi deserunt ratione eos ipsa? Laudantium
-              nobis eligendi sit libero possimus necessitatibus nulla, quam
-              tenetur aliquid incidunt obcaecati voluptates ipsa, magni,
-              praesentium minus iste quae dolorem aperiam. Sit, laborum enim
-              commodi provident deleniti non iste iusto sed inventore.
+              The Coaching ExcelleRater is a video-based decision-making
+              platform for performance analysis and coaching.
             </p>
+
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              facere animi quod excepturi deserunt ratione eos ipsa? Laudantium
-              nobis eligendi sit libero possimus necessitatibus nulla, quam
-              tenetur aliquid incidunt obcaecati voluptates ipsa, magni,
-              praesentium minus iste quae dolorem aperiam. Sit, laborum enim
-              commodi provident deleniti non iste iusto sed inventore.
+              Identifying, classifying and rating video clips has become a
+              crucial component of many professional sports coaching programs.
             </p>
+
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              facere animi quod excepturi deserunt ratione eos ipsa? Laudantium
-              nobis eligendi sit libero possimus necessitatibus nulla, quam
-              tenetur aliquid incidunt obcaecati voluptates ipsa, magni,
-              praesentium minus iste quae dolorem aperiam. Sit, laborum enim
-              commodi provident deleniti non iste iusto sed inventore.
+              The Coaching ExcelleRater allows coaching staff to select relevant
+              video clips and customise decision-making scenarios for their
+              athletes.
             </p>
+
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              facere animi quod excepturi deserunt ratione eos ipsa? Laudantium
-              nobis eligendi sit libero possimus necessitatibus nulla, quam
-              tenetur aliquid incidunt obcaecati voluptates ipsa, magni,
-              praesentium minus iste quae dolorem aperiam. Sit, laborum enim
-              commodi provident deleniti non iste iusto sed inventore.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              facere animi quod excepturi deserunt ratione eos ipsa? Laudantium
-              nobis eligendi sit libero possimus necessitatibus nulla, quam
-              tenetur aliquid incidunt obcaecati voluptates ipsa, magni,
-              praesentium minus iste quae dolorem aperiam. Sit, laborum enim
-              commodi provident deleniti non iste iusto sed inventore.
+              Features include:
+              <ul>
+                <li>
+                  A simple 3-2-1 scoring system, with user-defined regions
+                  indicating a set of good decisions.
+                </li>
+                <li>
+                  Real-time and above real-time video playback, to better
+                  simulate in-game cognitive demands.
+                </li>
+                <li>
+                  Separate athlete and coach roles, providing access to the
+                  decision-making scenarios and summary data.
+                </li>
+              </ul>
             </p>
           </div>
         </div>
